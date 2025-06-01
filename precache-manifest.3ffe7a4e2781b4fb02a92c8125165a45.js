442d2a48746d739a6977369bde043369ac158b0d
Your Name <kianoshsolheim@outlook.com>
2025-06-01 08:45:46 +0200

    Updates

---
+++ precache-manifest.3ffe7a4e2781b4fb02a92c8125165a45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791abe7e0ae2a8855bd7bddc31e57252",
+    "url": "/websitev5/index.html"
+  },
+  {
+    "revision": "cbb0f2f952e706238206",
+    "url": "/websitev5/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eefe73f6a004316866bb",
+    "url": "/websitev5/static/js/2.2acdb46e.chunk.js"
+  },
+  {
+    "revision": "ea862b4d7291d9d99556",
+    "url": "/websitev5/static/js/3.bd0f349a.chunk.js"
+  },
+  {
+    "revision": "cbb0f2f952e706238206",
+    "url": "/websitev5/static/js/main.67cf110e.chunk.js"
+  },
+  {
+    "revision": "8ce154f0f9f0b13e65d9",
+    "url": "/websitev5/static/js/runtime~main.46282b1b.js"
+  }
+]);
